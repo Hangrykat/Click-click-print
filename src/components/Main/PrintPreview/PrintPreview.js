7ca0675f5e12d6c19f3e62a2f789da46bb9e51b6
Bruno DeLucca <bruno@delucca.cl>
2020-09-21 09:09:31 +0200

    can exit the print preview pressing outside the box

--- src/components/Main/PrintPreview/PrintPreview.js
+++ src/components/Main/PrintPreview/PrintPreview.js
@@ -1,21 +1,29 @@
-import React, { useContext } from 'react'
-import '../../../shared-styles.css'
-import {StateContext} from "../../../context"
+import React, { useContext } from "react";
+import "../../../shared-styles.css";
+import { StateContext } from "../../../context";
 
 function PrintPreview() {
-    const { setShowModal, showModal } = useContext(StateContext);
+  const { setShowModal, showModal } = useContext(StateContext);
 
-    return (
-        <div className="print-preview">
-            { showModal ? (<div id="modal-wrap" className="visible"></div>) :(<div id="modal-wrap" ></div>)}
-            <div className="modal-window">
-                <button onClick={ () => setShowModal(false)}>Back</button>
-                <div className="modal-content">
-                  <h1>Print Preview</h1>
-                </div>
-            </div>
+  return (
+    <div className="print-preview">
+      {showModal ? (
+        <div
+          id="modal-wrap"
+          className="visible"
+          onClick={() => setShowModal(false)}
+        ></div>
+      ) : (
+        <div id="modal-wrap"></div>
+      )}
+      <div className="modal-window">
+        <button onClick={() => setShowModal(false)}>Back</button>
+        <div className="modal-content">
+          <h1>Print Preview</h1>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default PrintPreview
+export default PrintPreview;
