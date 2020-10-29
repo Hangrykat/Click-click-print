8a03e6ca8f928097969234f114a6ba0c2855d324
Bruno DeLucca <bruno@delucca.cl>
2020-10-29 12:03:45 +0100

    Updates

---
+++ precache-manifest.36c74c8f93d97e246b13055fac8fc691.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f12334e5331ad4d942946ecd9c21300",
+    "url": "/Click-Click-Print-Backup/index.html"
+  },
+  {
+    "revision": "89d68e8afc516b9cd8c7",
+    "url": "/Click-Click-Print-Backup/static/css/2.e5a56be0.chunk.css"
+  },
+  {
+    "revision": "2a581c3a246dd0691c70",
+    "url": "/Click-Click-Print-Backup/static/css/main.fd381595.chunk.css"
+  },
+  {
+    "revision": "89d68e8afc516b9cd8c7",
+    "url": "/Click-Click-Print-Backup/static/js/2.f4b93c69.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Click-Click-Print-Backup/static/js/2.f4b93c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a581c3a246dd0691c70",
+    "url": "/Click-Click-Print-Backup/static/js/main.b291371b.chunk.js"
+  },
+  {
+    "revision": "18a9aeb24a5a1a4cd5ff",
+    "url": "/Click-Click-Print-Backup/static/js/runtime-main.0042f1e4.js"
+  },
+  {
+    "revision": "73140ab5e6b25eb817766a08a792ae9b",
+    "url": "/Click-Click-Print-Backup/static/media/NFPA_704.73140ab5.svg"
+  },
+  {
+    "revision": "6a5fc102245036fec9b9aa97f7f05eb8",
+    "url": "/Click-Click-Print-Backup/static/media/add.6a5fc102.svg"
+  },
+  {
+    "revision": "7f835f2ca5fdab08ddcb2c9ac88d785a",
+    "url": "/Click-Click-Print-Backup/static/media/blueDiamond.7f835f2c.svg"
+  },
+  {
+    "revision": "0fba2d7630873595677666f5948ba5a4",
+    "url": "/Click-Click-Print-Backup/static/media/compressedgas.0fba2d76.svg"
+  },
+  {
+    "revision": "608c01d1bc6dafa2c9d3eb2f34980934",
+    "url": "/Click-Click-Print-Backup/static/media/compressedgas.608c01d1.svg"
+  },
+  {
+    "revision": "4fe605a5dbff2fd52ef7f7307e994d78",
+    "url": "/Click-Click-Print-Backup/static/media/corrosive.4fe605a5.svg"
+  },
+  {
+    "revision": "cf4c07f17056310179892d298d9c5045",
+    "url": "/Click-Click-Print-Backup/static/media/corrosive.cf4c07f1.svg"
+  },
+  {
+    "revision": "744df28d6f2bd48ba957b5c954e115da",
+    "url": "/Click-Click-Print-Backup/static/media/environmentdanger.744df28d.svg"
+  },
+  {
+    "revision": "fe8074c5a50949f1975a75ea85b29e71",
+    "url": "/Click-Click-Print-Backup/static/media/environmentdanger.fe8074c5.svg"
+  },
+  {
+    "revision": "5bff468f3bafd0890209c42d340734c8",
+    "url": "/Click-Click-Print-Backup/static/media/explosive.5bff468f.svg"
+  },
+  {
+    "revision": "62f04a179368e3cb4a603cffb033afc9",
+    "url": "/Click-Click-Print-Backup/static/media/explosive.62f04a17.svg"
+  },
+  {
+    "revision": "4daddb970ff0e9ca01423d9e882b026e",
+    "url": "/Click-Click-Print-Backup/static/media/flammable.4daddb97.svg"
+  },
+  {
+    "revision": "6159a2142d2d27012a997df0bd38d592",
+    "url": "/Click-Click-Print-Backup/static/media/flammable.6159a214.svg"
+  },
+  {
+    "revision": "3c7d121117c1174d43b1e0184195e0e5",
+    "url": "/Click-Click-Print-Backup/static/media/healthhazard.3c7d1211.svg"
+  },
+  {
+    "revision": "bdd3de92ab264569261b5e5dddf78577",
+    "url": "/Click-Click-Print-Backup/static/media/healthhazard.bdd3de92.svg"
+  },
+  {
+    "revision": "7b1e51545450b44a586750e4816cde85",
+    "url": "/Click-Click-Print-Backup/static/media/oxidationdanger.7b1e5154.svg"
+  },
+  {
+    "revision": "ed9023d1d56e3c46288abe250a17aa4c",
+    "url": "/Click-Click-Print-Backup/static/media/oxidationdanger.ed9023d1.svg"
+  },
+  {
+    "revision": "ee66ebd1eda709d1336d1b3d4c218567",
+    "url": "/Click-Click-Print-Backup/static/media/pencil.ee66ebd1.svg"
+  },
+  {
+    "revision": "cbc82c3c375c2ed14b22fa5878063361",
+    "url": "/Click-Click-Print-Backup/static/media/redDiamond.cbc82c3c.svg"
+  },
+  {
+    "revision": "a70efafd734d34148c827c941f96ec92",
+    "url": "/Click-Click-Print-Backup/static/media/toxic.a70efafd.svg"
+  },
+  {
+    "revision": "d0281c25bf5538ce0179c08409ffc348",
+    "url": "/Click-Click-Print-Backup/static/media/toxic.d0281c25.svg"
+  },
+  {
+    "revision": "1d3e4d069f8fe0806bc0318af9d83575",
+    "url": "/Click-Click-Print-Backup/static/media/trash.1d3e4d06.svg"
+  },
+  {
+    "revision": "1079e732d57a9b4b84e12ba98b493870",
+    "url": "/Click-Click-Print-Backup/static/media/warning.1079e732.svg"
+  },
+  {
+    "revision": "4c183612060c8c37ce6fa1e1db28dfdd",
+    "url": "/Click-Click-Print-Backup/static/media/warning.4c183612.svg"
+  },
+  {
+    "revision": "23bb6f7fc8d6690e2a052c47ed0fc2a4",
+    "url": "/Click-Click-Print-Backup/static/media/whiteDiamond.23bb6f7f.svg"
+  },
+  {
+    "revision": "d1743d4a6b617b37e5de1665e9b8c7e6",
+    "url": "/Click-Click-Print-Backup/static/media/yellowDiamond.d1743d4a.svg"
+  }
+]);
